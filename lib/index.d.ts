f18e04a8475798c1c03866625de4458f143f28ba
TheTrueGravity <charliearmstrong180gmail.com>
2023-04-14 12:30:22 +0100

    push

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,6 @@
 export * as Manager from './manager';
 export * as Bot from './bot/bot';
+export * as embeds from './bot/modules/embeds';
+export * as ICategory from './bot/interfaces/ICategory';
+export * as ICommand from './bot/interfaces/ICommand';
+export * as ITask from './bot/interfaces/ITask';
